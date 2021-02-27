21270b195abdc60b22ec6d4a76d1114bbeec1283
福原佑介 <yusuke@fukuharayuusukenoMacBook-Pro.local>
2021-02-27 20:29:15 +0900

    fix

--- config/application.rb
+++ config/application.rb
@@ -32,6 +32,8 @@ module App
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
+    config.api_only = false # TODO originally true
+
+    config.hosts.clear # ADDED
   end
 end
